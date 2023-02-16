a1565eb6625328dc8544f778b10555e610acc5d0
Amir Khani <soheyl637@gmail.com>
2023-02-16 21:20:26 +0000

    Update (auto commit)

--- Auto Github MyPhone.bat
+++ Auto Github MyPhone.bat
@@ -1,11 +1,22 @@
 @ECHO OFF
+CLS
+COLOR 8
 ECHO.
+SET MSG="Update (auto commit)"
+ECHO.##### Git Status:
 git status
 ECHO.
 ECHO.
+ECHO.##### Git Add Files:
 git add *
-git commit -m "Update (auto commit)"
-COLOR 1
+git ECHO.
+ECHO.
+ECHO.
+ECHO.##### Git Commit:
+ECHO. 				%MSG%:
+git commit -m %MSG%
+ECHO.
+COLOR 3
 git push
 COLOR 2
 ECHO.
